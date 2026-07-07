d004f22db3bd0eb04fe99705100e1797374c9275
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-07 11:33:50 -0600

    feat: Se añadieron validaciones para los archivos JSON

--- src/utils/writer.ts
+++ src/utils/writer.ts
@@ -3,9 +3,17 @@ import { Cliente } from "../models/Cliente";
 import { Producto } from "../models/Producto";
 
 export async function escribirClientes(clientes: Cliente[]) {
-    await writeFile("./src/data/Clientes.json", JSON.stringify(clientes, null, 2));
+    try {
+        await writeFile("./src/data/Clientes.json", JSON.stringify(clientes, null, 2));
+    } catch (error) {
+        console.error("Error al escribir el archivo de clientes:", error);
+    }
 }
 
 export async function escribirProductos(productos: Producto[]) {
-    await writeFile( "./src/data/Productos.json", JSON.stringify(productos, null, 2) );
+    try {
+        await writeFile("./src/data/Productos.json", JSON.stringify(productos, null, 2));
+    } catch (error) {
+        console.error("Error al escribir el archivo de productos:", error);
+    }
 }
