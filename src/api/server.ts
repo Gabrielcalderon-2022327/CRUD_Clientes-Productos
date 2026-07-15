217b7fc5c13a7cc96845412296d86cb32e46a7ce
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-15 11:46:38 -0600

    feat: Se agrego validacion de error interno

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,8 +2,15 @@ import { createServer } from "node:http";
 import { router } from "./router";
 const PORT = 3000;
 
-const server = createServer((req, res) => {
-    router(req, res);
+const server = createServer(async (req, res) => {
+    try {
+        await router(req, res);
+    } catch (error) {
+        console.error("Error interno:", error);
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Error interno del servidor"
+        }));
+    }
 });
 
 server.listen(PORT, () => {
