40c8008e5ada869d222909f1d99f3a4f15f3c05c
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-17 18:56:26 -0600

    feat: Se creo funcion iniciar server

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,22 +1,24 @@
-import { createServer } from "node:http";
+import { createServer} from "http";
 import { router } from "./router";
 const PORT = 3000;
 
-const server = createServer(async (req, res) => {
-    try {
-        await router(req, res);
-    } catch (error) {
-        console.error("Error interno:", error);
-        res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Error interno del servidor"
-        }));
-    }
-});
 
-server.listen(PORT, () => {
-    console.log("--------------------------------");
-    console.log("SERVER INCIADO");
-    console.log(`PUERTO: ${PORT}`);
-    console.log("URL: http://localhost:" + PORT);
-    console.log("--------------------------------");
-});   
+export function iniciarServer(): void {
+    const server = createServer(async (req, res) => {
+        try {
+            await router(req, res);
+        } catch (error) {
+            console.error('Error interno:', error);
+            res.writeHead(500, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ error: 'Error interno del servidor' }));
+        }
+    });
+
+    server.listen(PORT, () => {
+        console.log("--------------------------------");
+        console.log("SERVER INCIADO");
+        console.log(`PUERTO: ${PORT}`);
+        console.log("URL: http://localhost:" + PORT);
+        console.log("--------------------------------");
+    });
+}
